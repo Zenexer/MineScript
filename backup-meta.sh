ca5577efdcbf15dcd0614ecddbf325fa15c23ab6
Paul Buonopane <paul+git@earth2me.com>
2013-03-25 11:41:08 -0400

    Better Tekkit debug filtering.
    Signed-off-by: Paul Buonopane <paul+git@earth2me.com>

---
+++ backup-meta.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# vim: fdm=marker fmr={{{,}}} fenc=utf-8
+
+# Environment {{{1
+#
+#
+
+source "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")/environment.sh"
+
+
+# Run Backup {{{1
+#
+#
+
+backup_shell
+exit $?
+
--- configuration.sh
+++ configuration.sh
@@ -6,19 +6,19 @@ MC_CONFIG_INSTANCE_FOLDER_NAME="$MC_CONFIG_INSTANCE"
 MC_CONFIG_BACKUP_INSTANCE_FOLDER_NAME="$MC_CONFIG_INSTANCE"
 
 MC_CONFIG_UID='mc'
-MC_CONFIG_JAR='craftbukkit.jar'
+MC_CONFIG_JAR='tekkit.jar'
 MC_CONFIG_MIN_STACK='1G'
-MC_CONFIG_MAX_STACK='2816M'
+MC_CONFIG_MAX_STACK='4608M'
 MC_CONFIG_ADDITIONAL_JAVA_ARGS=''
 
 # CraftBukkit		recent		bukkit
 # CraftBukkit		old			bukkit-old
 # CraftBukkit		very old	vanilla
-# Tekkit			1.2.5		bukkit-old
+# Tekkit			1.2.5		tekkit
 # Forge				>1.2.5		forge
 # Vanilla			*			vanilla
 # Spigot			*			bukkit
-MC_CONFIG_FRAMEWORK='bukkit'
+MC_CONFIG_FRAMEWORK='tekkit'
 
 MC_CONFIG_TMUX_SESSION='minecraft'
 MC_CONFIG_TMUX_WINDOW="$MC_CONFIG_INSTANCE"
